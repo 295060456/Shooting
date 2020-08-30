e372abc7322568dfa583bad4ff45ba6923f2ca9c
Jobs <>
2020-08-30 18:02:28 +0700

    no message

--- Pod 不进去的/ZZCircleProgress/ZZCountingLabel.m
+++ Pod 不进去的/ZZCircleProgress/ZZCountingLabel.m
@@ -10,49 +10,49 @@
 
 @interface ZZCountingLabel ()
 
-@property (nonatomic, strong) CADisplayLink *playLink;
-@property (nonatomic, assign) NSInteger displayPerSecond;
+@property(nonatomic,strong)CADisplayLink *playLink;
+@property(nonatomic,assign)NSInteger displayPerSecond;
 
-@property (nonatomic, assign) CGFloat fromValue;
-@property (nonatomic, assign) CGFloat toValue;
+@property(nonatomic,assign)CGFloat fromValue;
+@property(nonatomic,assign)CGFloat toValue;
 
-@property (nonatomic, assign) CGFloat increaseValue;
-@property (nonatomic, assign) CGFloat perValue;
+@property(nonatomic,assign)CGFloat increaseValue;
+@property(nonatomic,assign)CGFloat perValue;
 
 @end
 
 @implementation ZZCountingLabel
 
-- (instancetype)init {
+-(instancetype)init {
     if (self = [super init]) {
         [self initValues];
-    }
-    return self;
+    }return self;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame {
+-(instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         [self initValues];
-    }
-    return self;
+    }return self;
 }
 
-- (void)awakeFromNib {
+-(void)awakeFromNib {
     [super awakeFromNib];
     [self initValues];
 }
 
-- (void)initValues {
+-(void)initValues {
     _duration = 2.0;
     _displayPerSecond = 30;
 }
 
-- (void)countingFrom:(CGFloat)fromValue
+-(void)countingFrom:(CGFloat)fromValue
                   to:(CGFloat)toValue {
-    [self countingFrom:fromValue to:toValue duration:_duration];
+    [self countingFrom:fromValue
+                    to:toValue
+              duration:_duration];
 }
 
-- (void)countingFrom:(CGFloat)fromValue
+-(void)countingFrom:(CGFloat)fromValue
                   to:(CGFloat)toValue
             duration:(CGFloat)duration {
     
@@ -68,17 +68,17 @@
         self.playLink = nil;
     }
     
-    CADisplayLink *playLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(countingAction)];
+    CADisplayLink *playLink = [CADisplayLink displayLinkWithTarget:self
+                                                          selector:@selector(countingAction)];
     if (@available(iOS 10.0, *)) {
         playLink.preferredFramesPerSecond = _displayPerSecond;
     }else{
         playLink.frameInterval = 60 / (_displayPerSecond == 0 ? 1 : _displayPerSecond);
     }
     
-    [playLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-    [playLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:UITrackingRunLoopMode];
+    [playLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];//定时器只运行在NSDefaultRunLoopMode下，一旦RunLoop进入其他模式，这个定时器就不会工作
+    [playLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:UITrackingRunLoopMode];//界面跟踪 Mode，用于 ScrollView 追踪触摸滑动，保证界面滑动时不受其他 Mode 影响
     self.playLink = playLink;
-    
 }
 
 - (void)countingAction {
@@ -93,22 +93,20 @@
             [self stopDisplayLink];
         }
     }
-    
     dispatch_async(dispatch_get_main_queue(), ^{
         self.text = [NSString isNullString:self.placeStr] ? [NSString stringWithFormat:@"%.0f%%",self->_increaseValue] : self.placeStr;
     });
 }
 
-- (void)stopDisplayLink {
-    
+-(void)stopDisplayLink {
     [self.playLink invalidate];
     self.playLink = nil;
-    
     _increaseValue = _toValue;
 }
 
 -(void)setPlaceStr:(NSString *)placeStr{
-    self.text = placeStr;
+    _placeStr = placeStr;
+    self.text = _placeStr;
 }
 
 @end
--- Shooting/🇨🇳ServiceLogic/拍照/拍摄/拍摄页面的一切其他的子控件/StartOrPauseBtn.m
+++ Shooting/🇨🇳ServiceLogic/拍照/拍摄/拍摄页面的一切其他的子控件/StartOrPauseBtn.m
@@ -72,14 +72,12 @@
     [_mytimer invalidate];
     _mytimer = nil;
     
-//    self.currentTime = 0;
-//    self.progressView.increaseFromLast = NO;
-//    self.progressView.progress = 0;
-//    self.progressView.startAngle = 0.1;
-//    self.progressView.startAngle = 0;
-//    self.progressView.increaseFromLast = YES;
-    
-    
+    self.currentTime = 0;
+    self.progressView.increaseFromLast = NO;
+    self.progressView.progress = 0;
+    self.progressView.startAngle = 0.1;
+    self.progressView.startAngle = 0;
+    self.progressView.increaseFromLast = YES;
 }
 
 -(void)tapGRUI:(BOOL)isClick{
