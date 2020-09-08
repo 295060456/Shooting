03e995e6e900a1b14dcc606f3d074d2038f88572
Jobs <>
2020-09-08 12:48:54 +0700

    no message

--- Pod 不进去的/ZZCircleProgress/ZZCircleProgress.m
+++ Pod 不进去的/ZZCircleProgress/ZZCircleProgress.m
@@ -10,8 +10,9 @@
 
 @interface ZZCircleProgress ()<CAAnimationDelegate>
 
-@property(nonatomic,strong)CAShapeLayer *backLayer;
-@property(nonatomic,strong)CAShapeLayer *progressLayer;
+@property(nonatomic,strong)CAShapeLayer *pointLayer;//控制点
+@property(nonatomic,strong)CAShapeLayer *backLayer;//跑道
+@property(nonatomic,strong)CAShapeLayer *progressLayer;//已经走过的距离
 
 @property(nonatomic,assign)CGFloat realWidth;//实际边长
 @property(nonatomic,assign)CGFloat radius;//半径
@@ -22,25 +23,24 @@
 
 @implementation ZZCircleProgress
 
-- (instancetype)init {
+-(instancetype)init {
     if (self = [super init]) {
         [self initialization];
     }return self;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame {
+-(instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         [self initialization];
     }return self;
 }
 
-- (void)awakeFromNib {
+-(void)awakeFromNib {
     [super awakeFromNib];
     [self initialization];
 }
-
 //初始化
-- (instancetype)initWithFrame:(CGRect)frame
+-(instancetype)initWithFrame:(CGRect)frame
                 pathBackColor:(UIColor *)pathBackColor
                 pathFillColor:(UIColor *)pathFillColor
                    startAngle:(CGFloat)startAngle
@@ -54,9 +54,8 @@
     }
     return self;
 }
-
 //初始化数据
-- (void)initialization {
+-(void)initialization {
     self.backgroundColor = [UIColor clearColor];
     _pathBackColor = [UIColor lightGrayColor];
     _pathFillColor = [UIColor redColor];
@@ -72,36 +71,46 @@
     //初始化layer
     [self initSubviews];
 }
-
 #pragma Get
-- (CAShapeLayer *)backLayer {
+-(CAShapeLayer *)backLayer {
     if (!_backLayer) {
         _backLayer = [CAShapeLayer layer];
         _backLayer.fillColor = [UIColor clearColor].CGColor;//填充色
         _backLayer.lineWidth = _strokeWidth;
         _backLayer.strokeColor = _pathBackColor.CGColor;
-        _backLayer.lineCap = @"round";
+        _backLayer.lineCap = kCALineCapSquare;
     }
     return _backLayer;
 }
 
-- (CAShapeLayer *)progressLayer {
+-(CAShapeLayer *)pointLayer{
+    if (!_pointLayer) {
+        _pointLayer = [CAShapeLayer layer];
+        _pointLayer.fillColor = [UIColor redColor].CGColor;//填充色
+        _pointLayer.lineWidth = _strokeWidth;
+        _pointLayer.strokeColor = [UIColor blueColor].CGColor;
+        _pointLayer.lineCap = kCALineCapSquare;
+    }return _pointLayer;
+}
+
+-(CAShapeLayer *)progressLayer {
     if (!_progressLayer) {
         _progressLayer = [CAShapeLayer layer];
         _progressLayer.fillColor = [UIColor clearColor].CGColor;//填充色
         _progressLayer.lineWidth = _strokeWidth;
         _progressLayer.strokeColor = _pathFillColor.CGColor;
-        _progressLayer.lineCap = @"round";
+        _progressLayer.lineCap = kCALineCapSquare;
     }
     return _progressLayer;
 }
 
-- (UIImageView *)pointImage {
+-(UIImageView *)pointImage {
     if (!_pointImage) {
         _pointImage = [[UIImageView alloc] init];
         
         NSBundle *mainBundle = [NSBundle bundleForClass:[self class]];
-        NSBundle *resourcesBundle = [NSBundle bundleWithPath:[mainBundle pathForResource:@"ZZCircleProgress" ofType:@"bundle"]];
+        NSBundle *resourcesBundle = [NSBundle bundleWithPath:[mainBundle pathForResource:@"ZZCircleProgress"
+                                                                                  ofType:@"bundle"]];
         _pointImage.image = [UIImage imageNamed:@"circle_point1"
                                        inBundle:resourcesBundle
                   compatibleWithTraitCollection:nil];
@@ -109,9 +118,9 @@
     return _pointImage;
 }
 
-- (ZZCountingLabel *)progressLabel {
+-(ZZCountingLabel *)progressLabel {
     if (!_progressLabel) {
-        _progressLabel = [[ZZCountingLabel alloc] init];
+        _progressLabel = ZZCountingLabel.new;
         _progressLabel.textColor = [UIColor blackColor];
         _progressLabel.textAlignment = NSTextAlignmentCenter;
         _progressLabel.font = [UIFont systemFontOfSize:22];
@@ -119,9 +128,8 @@
     }
     return _progressLabel;
 }
-
 #pragma Set
-- (void)setStartAngle:(CGFloat)startAngle {
+-(void)setStartAngle:(CGFloat)startAngle {
     if (_startAngle != ZZCircleDegreeToRadian(startAngle)) {
         _startAngle = ZZCircleDegreeToRadian(startAngle);
     }else{
@@ -130,7 +138,7 @@
     [self setNeedsLayout];
 }
 
-- (void)setReduceAngle:(CGFloat)reduceAngle {
+-(void)setReduceAngle:(CGFloat)reduceAngle {
     if (_reduceAngle != ZZCircleDegreeToRadian(reduceAngle)) {
         if (reduceAngle>=360) {
             return;
@@ -140,7 +148,7 @@
     }
 }
 
-- (void)setStrokeWidth:(CGFloat)strokeWidth {
+-(void)setStrokeWidth:(CGFloat)strokeWidth {
     if (_strokeWidth != strokeWidth) {
         _strokeWidth = strokeWidth;
         _radius = _realWidth/2.0 - _strokeWidth/2.0;
@@ -148,27 +156,27 @@
     }
 }
 
-- (void)setPathBackColor:(UIColor *)pathBackColor {
+-(void)setPathBackColor:(UIColor *)pathBackColor {
     if (_pathBackColor != pathBackColor) {
         _pathBackColor = pathBackColor;
         self.backLayer.strokeColor = _pathBackColor.CGColor;
     }
 }
 
-- (void)setPathFillColor:(UIColor *)pathFillColor {
+-(void)setPathFillColor:(UIColor *)pathFillColor {
     if (_pathFillColor != pathFillColor) {
         _pathFillColor = pathFillColor;
         self.progressLayer.strokeColor = _pathFillColor.CGColor;
     }
 }
 
-- (void)setShowPoint:(BOOL)showPoint {
+-(void)setShowPoint:(BOOL)showPoint {
     if (_showPoint != showPoint) {
         _showPoint = showPoint;
         if (_showPoint) {
             self.pointImage.hidden = NO;
             [self updatePointPosition];
-        } else {
+        }else {
             self.pointImage.hidden = YES;
         }
     }
@@ -179,13 +187,13 @@
         _showProgressText = showProgressText;
         if (_showProgressText) {
             self.progressLabel.hidden = NO;
-        } else {
+        }else {
             self.progressLabel.hidden = YES;
         }
     }
 }
 
-- (void)setProgress:(CGFloat)progress {
+-(void)setProgress:(CGFloat)progress{
     _progress = MAX(MIN(1, progress), 0);
     
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -193,13 +201,14 @@
     });
 }
 #pragma Methods
-- (void)startAnimation {
-    
-    [self.progressLayer addAnimation:self.pathAnimation forKey:@"strokeEndAnimation"];
+-(void)startAnimation{
+    [self.progressLayer addAnimation:self.pathAnimation
+                              forKey:@"strokeEndAnimation"];
     
     if (_showPoint) {
         CAAnimation *pointAnimation = self.pointAnimation;
-        [self.pointImage.layer addAnimation:pointAnimation forKey:@"pointAnimation"];
+        [self.pointImage.layer addAnimation:pointAnimation
+                                     forKey:@"pointAnimation"];
         self.lastPointAnimation = pointAnimation;
         
         if (!_increaseFromLast && _progress == 0.0) {
@@ -208,9 +217,13 @@
     }
     
     if (_showProgressText && _increaseFromLast) {
-        [self.progressLabel countingFrom:_lastProgress*100 to:_progress*100 duration:_duration];
+        [self.progressLabel countingFrom:_lastProgress*100
+                                      to:_progress*100
+                                duration:_duration];
     } else if (_showProgressText) {
-        [self.progressLabel countingFrom:0 to:_progress*100 duration:_duration];
+        [self.progressLabel countingFrom:0
+                                      to:_progress*100
+                                duration:_duration];
     }
     
     _lastProgress = _progress;
@@ -221,17 +234,19 @@
     [self.pointImage.layer removeAllAnimations];
     [self.progressLayer removeAllAnimations];
     
-    [self.progressLayer addAnimation:self.pathResetAnimation forKey:@"strokeResetAnimation"];
+    [self.progressLayer addAnimation:self.pathResetAnimation
+                              forKey:@"strokeResetAnimation"];
     
     if (_showPoint) {
         CAAnimation *pointAnimation = self.pointResetAnimation;
-        [self.pointImage.layer addAnimation:pointAnimation forKey:@"pointResetAnimation"];
+        [self.pointImage.layer addAnimation:pointAnimation
+                                     forKey:@"pointResetAnimation"];
         self.lastPointAnimation = pointAnimation;
     }
 }
 //下面这2个不能写成属性，因为要反复绘制
 //线的动画
-- (CAAnimation *)pathAnimation {
+-(CAAnimation *)pathAnimation {
     CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     pathAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];//设置动画的速度变化
     pathAnimation.duration = _duration;//动画的时长
@@ -242,7 +257,7 @@
     return pathAnimation;
 }
 //点的动画
-- (CAAnimation *)pointAnimation {
+-(CAAnimation *)pointAnimation {
     CAKeyframeAnimation *pointAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
     pointAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];//设置动画的速度变化
     pointAnimation.fillMode = kCAFillModeForwards;
@@ -264,7 +279,7 @@
     return pointAnimation;
 }
 
-- (CAAnimation *)pathResetAnimation {
+-(CAAnimation *)pathResetAnimation {
     CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeReset"];
     pathAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];//设置动画的速度变化
     pathAnimation.duration = _duration;//动画的时长
@@ -275,7 +290,7 @@
     return pathAnimation;
 }
 //点的动画
-- (CAAnimation *)pointResetAnimation {
+-(CAAnimation *)pointResetAnimation {
     CAKeyframeAnimation *pointAnimation = [CAKeyframeAnimation animationWithKeyPath:@"positionReset"];
     pointAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];//设置动画的速度变化
     pointAnimation.fillMode = kCAFillModeForwards;
@@ -289,7 +304,7 @@
     }
     UIBezierPath *imagePath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(_realWidth/2.0, _realWidth/2.0)
                                                              radius:_radius
-                                                         startAngle:_increaseFromLast==YES ? (2 * M_PI - _reduceAngle) * _lastProgress + _startAngle : _startAngle
+                                                         startAngle:_increaseFromLast == YES ? (2 * M_PI - _reduceAngle) * _lastProgress + _startAngle : _startAngle
                                                            endAngle:0
                                                           clockwise:!clockwise];
     pointAnimation.path = imagePath.CGPath;
@@ -297,7 +312,7 @@
     return pointAnimation;
 }
 
-- (UIBezierPath *)getNewBezierPath {
+-(UIBezierPath *)getNewBezierPath {
     return [UIBezierPath bezierPathWithArcCenter:CGPointMake(_realWidth/2.0, _realWidth/2.0)
                                           radius:_radius
                                       startAngle:_startAngle
@@ -305,14 +320,22 @@
                                        clockwise:YES];
 }
 
+-(UIBezierPath *)getPointBezierPath {
+    return [UIBezierPath bezierPathWithArcCenter:CGPointMake(_realWidth/2.0, _realWidth/2.0)
+                                          radius:_radius
+                                      startAngle:4
+                                        endAngle:4.001
+                                       clockwise:YES];
+}
 
-- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+- (void)animationDidStop:(CAAnimation *)anim
+                finished:(BOOL)flag {
     if (flag && anim == [self.pointImage.layer animationForKey:@"pointAnimation"]) {
         [self updatePointPosition];
     }
 }
 
-- (void)updatePointPosition {
+-(void)updatePointPosition {
     CGFloat currentEndAngle = (2*M_PI-_reduceAngle)*_progress+_startAngle;
     [_pointImage.layer removeAllAnimations];
     _pointImage.center = CGPointMake(_realWidth / 2.0 + _radius * cosf(currentEndAngle),
@@ -320,35 +343,55 @@
 }
 
 #pragma initSubviews
-- (void)initSubviews {
-    [self.layer addSublayer:self.backLayer];
-    [self.layer addSublayer:self.progressLayer];
+-(void)initSubviews {
+    
+    [self.layer addSublayer:self.backLayer];//跑道
+    [self.layer addSublayer:self.pointLayer];//点
+    [self.layer addSublayer:self.progressLayer];//已经走过的距离
     
     [self addSubview:self.pointImage];
     [self addSubview:self.progressLabel];
 }
 
 #pragma layout
-- (void)layoutSubviews {
+-(void)layoutSubviews {
     [super layoutSubviews];
     
     self.realWidth = MIN(ZZCircleSelfWidth, ZZCircleSelfHeight);
     self.radius = _realWidth/2.0 - _strokeWidth/2.0;
     
-    self.backLayer.frame = CGRectMake(0, 0, _realWidth, _realWidth);
+    self.pointLayer.frame = CGRectMake(0,
+                                       0,
+                                       _realWidth,
+                                       _realWidth);
+    self.pointLayer.lineWidth = _strokeWidth;
+    self.pointLayer.path = [self getPointBezierPath].CGPath;
+    
+    self.backLayer.frame = CGRectMake(0,
+                                      0,
+                                      _realWidth,
+                                      _realWidth);
     self.backLayer.lineWidth = _strokeWidth;
     self.backLayer.path = [self getNewBezierPath].CGPath;
 
-    self.progressLayer.frame = CGRectMake(0, 0, _realWidth, _realWidth);
+    self.progressLayer.frame = CGRectMake(0,
+                                          0,
+                                          _realWidth,
+                                          _realWidth);
     self.progressLayer.lineWidth = _strokeWidth;
     self.progressLayer.path = [self getNewBezierPath].CGPath;
     self.progressLayer.strokeEnd = 0.0;
 
-    self.progressLabel.frame = CGRectMake(0, 0, _realWidth, _realWidth);
+    self.progressLabel.frame = CGRectMake(0,
+                                          0,
+                                          _realWidth,
+                                          _realWidth);
     
-    self.pointImage.frame = CGRectMake(0, 0, _strokeWidth, _strokeWidth);
+    self.pointImage.frame = CGRectMake(0,
+                                       0,
+                                       _strokeWidth,
+                                       _strokeWidth);
     [self updatePointPosition];
-        
 }
 
 @end
