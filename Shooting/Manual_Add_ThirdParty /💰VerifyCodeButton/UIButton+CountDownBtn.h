415583eb573820137d62f3349ed1b32b50e94aa5
Jobs <>
2020-09-14 23:25:32 +0700

    no message

--- Shooting/Manual_Add_ThirdParty /💰VerifyCodeButton/UIButton+CountDownBtn.h
+++ Shooting/Manual_Add_ThirdParty /💰VerifyCodeButton/UIButton+CountDownBtn.h
@@ -44,19 +44,20 @@ NS_ASSUME_NONNULL_BEGIN
  */
 
 @interface UIButton (CountDownBtn)
-
-@property(nonatomic,strong)NSTimerManager *nsTimerManager;
+/* 在初始化方法里面赋值 */
 @property(nonatomic,strong)NSString *titleBeginStr;
-@property(nonatomic,strong)NSString *titleRuningStr;//倒计时过程中显示的非时间文字
-@property(nonatomic,strong)NSString *titleEndStr;
 @property(nonatomic,strong)UIColor *titleColor;
-//倒计时开始前的背景色直接对此控件进行赋值 backgroundColor
-@property(nonatomic,strong)UIColor *bgCountDownColor;//倒计时的时候此btn的背景色
-@property(nonatomic,strong)UIColor *bgEndColor;//倒计时完全结束后的背景色
 @property(nonatomic,strong)UIColor *layerBorderColor;
 @property(nonatomic,strong)UIFont *titleLabelFont;
 @property(nonatomic,assign)CGFloat layerCornerRadius;
 @property(nonatomic,assign)CGFloat layerBorderWidth;
+
+@property(nonatomic,strong)NSTimerManager *nsTimerManager;
+@property(nonatomic,strong)NSString *titleRuningStr;//倒计时过程中显示的非时间文字
+@property(nonatomic,strong)NSString *titleEndStr;
+//倒计时开始前的背景色直接对此控件进行赋值 backgroundColor
+@property(nonatomic,strong)UIColor *bgCountDownColor;//倒计时的时候此btn的背景色
+@property(nonatomic,strong)UIColor *bgEndColor;//倒计时完全结束后的背景色
 @property(nonatomic,assign)long count;// 倒计时
 @property(nonatomic,assign)ShowTimeType showTimeType;//时间显示风格
 @property(nonatomic,assign)CountDownBtnRunType btnRunType;
