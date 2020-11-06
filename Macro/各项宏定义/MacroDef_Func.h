b1f579218807d519bb6468ee3c86fa83e6fd557a
Jobs <lg295060456@gmail.com>
2020-11-06 21:07:43 +0700

    no message

--- Macro/各项宏定义/MacroDef_Func.h
+++ Macro/各项宏定义/MacroDef_Func.h
@@ -8,9 +8,8 @@
 #ifndef MacroDef_Func_h
 #define MacroDef_Func_h
 
-#import "SceneDelegate.h"
 #import "MacroDef_Sys.h"
-
+#import "SceneDelegate.h"
 static inline UIWindow * getMainWindow(){
     UIWindow *window = nil;
     if (@available(iOS 13.0, *)) {
@@ -45,15 +44,6 @@ static inline id getSceneDelegate(){
     }return sceneDelegate;
 }
 
-static inline CGFloat Top(){
-    static CGFloat value = 0;
-    static dispatch_once_t once_t = 0;
-    dispatch_once(&once_t, ^{
-        value = isiPhoneX_series() ? 88.0f : 64.0f;
-    });
-    return value;
-}
-
 #pragma mark ======================================== 字体 ================================================
 #define kFontSize(x) [UIFont systemFontOfSize:x weight:UIFontWeightRegular]
 
@@ -71,7 +61,6 @@ static inline CGFloat Top(){
 #define GetUserDefaultBoolForKey(key) [[NSUserDefaults standardUserDefaults] boolForKey:key]
 #define DeleUserDefaultWithKey(key) [[NSUserDefaults standardUserDefaults] removeObjectForKey:key]
 #define UserDefaultSynchronize  [[NSUserDefaults standardUserDefaults] synchronize]
-
 #pragma mark ======================================== 默认值 ========================================
 #define defaultValue 0
 #define defaultObj Nil
@@ -96,7 +85,7 @@ return; \
 #pragma mark ======================================== 其他 =================================================
 #define ReuseIdentifier NSStringFromClass ([self class])
 #define CurrentThread [NSThread currentThread]
-#define PrintRetainCount(obj) printf("Retain Count = %ld\n",CFGetRetainCount((__bridge CFTypeRef)(obj)));
+#define PrintRetainCount(obj) printf("Retain Count = %ld\n",CFGetRetainCount((__bridge CFTypeRef)(obj)));//打印引用计数器
 //- (void)add:(NSString *)txt{}
 #define addText(fmt, ...) [self add:[NSString stringWithFormat:fmt, ##__VA_ARGS__]]//多参数
 
