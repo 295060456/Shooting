f8e18a76413626fe2a34fc24c2526fd3ed353145
Jobs <lg295060456@gmail.com>
2021-01-15 10:19:55 +0700

    no message

--- Shooting/🇨🇳ServiceLogic/Common/数据模型层/BaseModel.m
+++ Shooting/🇨🇳ServiceLogic/Common/数据模型层/BaseModel.m
@@ -9,4 +9,18 @@
 
 @implementation BaseModel
 
+//+(NSDictionary *)mj_replacedKeyFromPropertyName{
+//    /* 返回的字典，key为模型属性名，value为转化的字典的多级key */
+//    return @{
+//        @"ID" : @"id",
+//        @"childMutArr":@"child"
+//    };
+//}
+//
+//+(NSDictionary *)mj_objectClassInArray{
+//    return @{
+//        @"childMutArr" : MKChildCommentModel.class
+//    };
+//}
+
 @end
